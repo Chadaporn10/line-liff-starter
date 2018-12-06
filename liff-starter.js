667d7240f4869159c55253cdd2b060d792640bbf
Chadaporn10 <45582340+Chadaporn10@users.noreply.github.com>
2018-12-06 18:07:14 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,12 @@
 window.onload = function (e) {
 	
     liff.init(function (data) {
+	    liff.getProfile().then(function (profile) {
+		document.getElementById('displayimg').textContent = profile.userId;
+		document.getElementById('displayname').textContent = profile.displayName;
+	}).catch(function (error) {
+		window.alert("Error getting profile: " + error);
+	});
         initializeApp(data);
 	    test();
     });
