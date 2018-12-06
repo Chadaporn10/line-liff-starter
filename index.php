8ff331241b53849d302061e23409cd2c256ed92a
Chadaporn10 <45582340+Chadaporn10@users.noreply.github.com>
2018-12-06 14:24:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+//header( 'Location: /index.html' ) ;  
+echo "chadaporn Ma...La..Ja";
+?>
